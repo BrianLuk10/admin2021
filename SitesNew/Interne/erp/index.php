4119637a69d3004df8bd7ace57af8277587489d5
GGEphec <he201732@students.ephec.be>
2021-08-06 00:21:48 -0700

    maj web

---
+++ SitesNew/Interne/erp/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>ERP WoodyToys</title>
+	<meta charset="utf-8" />
+</head>
+
+<body>
+	<h2>Bienvenue sur l'ERP de WoodyToys</h2>
+
+	<p>Connectez-vous au moyen de vos identifiants : </p>
+	<form action="" method="post" name="Connexion_employés">
+		<label>Identifiant</label>
+		<input type="text" name="identifiantEmploye" required>
+		<label>Mot de passe</label>
+		<input type="password" name="mdpEmploye" required>
+		<button type="submit" name="connexion" value="connexion">Connexion</button>
+	</form>
+	<?php
+		session_start();
+		include('config.php');
+		if (isset($_POST['connexion'])) {
+			$identifiant = $_POST['identifiantEmploye'];
+			$mdp = $_POST['mdpEmploye'];
+			$query = $connection->prepare("SELECT * FROM employes WHERE identifiantEmploye=:identifiant");
+			$query->bindParam("identifiant", $identifiant, PDO::PARAM_STR);
+			$query->execute();
+			$result = $query->fetch(PDO::FETCH_ASSOC);
+			if (!$result) {
+				echo '<p class="error">Vous n\'existez pas dans la base de données</p>';
+			} else {
+				if (password_verify($mdp, $result['hashMDPEmploye'])) {
+					$_SESSION['user_id'] = $result['idEmploye'];
+					echo '<p class="success">Bienvenue </p>';
+				} else {
+					echo '<p class="error">Mauvais mot de passe</p>';
+				}
+			}
+		}
+	?>
+
+</body>
+</html>
